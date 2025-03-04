fef9d1eb05c9f8d9a5efbdf37735667534c10f92
ginpks <ginhpark@gmail.com>
2025-03-03 23:01:33 -0500

    removed unused function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,15 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 //  new router object to hold routes related to users.
-const { createUser, getAllUsers, getUserById } = require('../controllers/userController');
+const { createUser, getAllUsers } = require('../controllers/userController');
 
 //create a user
 router.post('/', createUser);
 
 //get all users
 router.get('/', getAllUsers);
-
-//get a user by id
-router.get('/:id', getUserById);
 
 module.exports = router;
