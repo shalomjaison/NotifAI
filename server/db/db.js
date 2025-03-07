ee81bcaff8ff801838e0bd67e479835ba5661b55
ginpks <ginhpark@gmail.com>
2025-03-07 15:14:17 -0500

    IMPORTED SEQUELIZE AND ESTABLISHED DATABASE CONNECTION

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,27 @@
 // Importing node-postgres library. 
 // The pg library is the bridge that allows your Node.js code to "talk" to the PostgreSQL server
 // It sends commands to the database and receives results back.
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
 
-
-const pool = new Pool({ // establishes new connection pool to the postgreSQL database
-    host: "localhost", 
-    user: "postgres", 
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
     port: 5432,
-    password: "256325", // replace with your password that you set during postgreSQL installation
-    database: "users"
-});
+    username: 'postgres',
+    password: '256325',
+    database: 'users',
+    logging: false,
+})
 
-// Optional: Test the connection (can be removed later)
-pool.connect()
-    .then(() => console.log('Connected to database successfully!'))
-    .catch(err => console.error('Error connecting to database:', err.message));
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to database established wahoo!');
+    } catch (error) {
+        console.log('Unable to connect to the database womp womp', error);
+    }
+}
 
-module.exports = pool; // Export the pool directly
+testConnection();
+
+module.exports = sequelize;
