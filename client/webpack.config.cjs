48560904a11d50f34d8e92b14956121b543a9161
joshuagu789 <joshuagu789@gmail.com>
2025-03-23 20:00:12 -0400

    added webpack support for loading css

--- client/webpack.config.cjs
+++ client/webpack.config.cjs
@@ -59,7 +59,7 @@ module.exports={
          * resolve the one with the extension listed first in the array and skip the rest. 
          * This is what enables users to leave off the extension when importing
          */
-        extensions: ['.js','.jsx','.json'] 
+        extensions: ['.js','.jsx','.json', '.css'] 
     },
     module:{
         /** "rules"
@@ -68,12 +68,18 @@ module.exports={
          * add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from 
          * being searched"
          */
+
         rules: [
             {
                 test: /\.(js|jsx)$/,    //kind of file extension this rule should look for and apply in test
                 exclude: /node_modules/, //folder to be excluded
                 use:  'babel-loader' //loader which we are going to use
-            }
+            },
+
+            {
+                test: /\.css$/i,    // for loading css files
+                use: ["style-loader", "css-loader"],
+            },
         ]
     },
 }
