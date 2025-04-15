4e8a9d2870e22ca842994e56dbdb6677f8478f0c
joshuagu789 <joshuagu789@gmail.com>
2025-04-14 20:47:58 -0400

    added backend api implementation for get single notification

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -9,12 +9,14 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
 const { getNotifications } = require('../controllers/getNotificationsController');
+const { getNotification } = require('../controllers/getNotificationController');
 const { createNotification } = require('../controllers/createNotificationController');
 
 
 const router = express.Router();
 
-router.post('/', authMiddleware, getNotifications); // auth middleware executed before getNotifications
+router.post('/', authMiddleware, getNotifications); // auth middleware executed before getNotifications- note this gets all notifications by filter/sort
+router.get('/:id', authMiddleware, getNotification); // auth middleware executed before getNotification- note getNotification gets 1 notification by id
 router.post('/create', authMiddleware, createNotification); // auth middleware executed before createNotification
 
 
