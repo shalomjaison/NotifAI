a3246b0f6e9a9d3ed32360079c270ef8e3da375d
HyunSeoJee <hyunsjee@gmail.com>
2025-05-07 04:30:12 -0400

    fixing search username

--- client/src/MainPageComponents/SearchRecipient/SearchRecipient.js
+++ client/src/MainPageComponents/SearchRecipient/SearchRecipient.js
@@ -10,24 +10,17 @@ const SearchRecipient = ({ onSearch = () => {}}) => {
 
   const validUsername = async (username) => {
     try {
-      // Log the username and the API call to make sure it's being triggered correctly
       console.log(`Validating username: ${username}`);
       
-      // Make the request to the backend to check if the username exists
       const res = await axios.get(`${backendBaseURL}/users/exists/${username}`, {
-        withCredentials: true, // Add this if you need to send cookies
+        withCredentials: true,
       });
-  
-      // Log the API response to check if it's working correctly
       console.log(`API Response:`, res.data);
   
-      // Check the response from the backend to see if the username exists
       return res.data.exists;
     } catch (err) {
-      // Log the error if something goes wrong
       console.error('Validation error:', err);
       
-      // Return false if an error occurs
       return false;
     }
   };
